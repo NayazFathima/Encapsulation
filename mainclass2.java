8cf13df3d4f6c7067de9a5a825db962b768c4e23
NayazFathima <123888169+NayazFathima@users.noreply.github.com>
2023-02-01 10:55:13 +0530

    Add files via upload

---
+++ mainclass2.java
@@ -0,0 +1,10 @@
+package encapsulation;
+
+public class mainclass2 {
+
+	public static void main(String[] args) {
+		one o= new one();
+		o.setB(6);
+		System.out.println(o.getB());
+		}
+	}
---
+++ one.java
@@ -0,0 +1,13 @@
+package encapsulation;
+
+public class one {
+	private int b;
+
+	public int getB() {
+		return b;
+	}
+
+	public void setB(int b) {
+		this.b = b;
+	}
+	}
